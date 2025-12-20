68d4e416bbf95feed3e53aa8d2d0e6e25f571880
Mohsen Abbasi <mabbasidesign@yahoo.com>
2025-12-19 19:17:55 -0500

    Use InventoryItem model in API

--- src/inventory-service/Program.cs
+++ src/inventory-service/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using InventoryService.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -26,5 +27,3 @@ app.MapDelete("/inventory/{id}", (string id) => {
 });
 
 app.Run();
-
-record InventoryItem(string Id, string ProductName, int Quantity, string Location);
